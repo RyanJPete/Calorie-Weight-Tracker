df0a5cde11b5a72f7410b115dd36a24a0614a581
RyanJPete <ryanjp@onlinenw.com>
2021-04-18 06:24:37 -0700

    Eat button in EatActivity updates calorie count for the day

--- app/src/main/java/com/example/fitnesstracker/EatActivity.java
+++ app/src/main/java/com/example/fitnesstracker/EatActivity.java
@@ -51,7 +51,7 @@ public class EatActivity extends AppCompatActivity {
 
         ingredients.setAdapter(adapter);
 
-        ingredients.setSelection(0,false);
+        //ingredients.setSelection(0,false);
         ingredients.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
@@ -94,35 +94,28 @@ public class EatActivity extends AppCompatActivity {
     }
 
     public void eat (View view){
-    /*    TextView txt = findViewById(R.id.eatTxt);
+        TextView txt = findViewById(R.id.totalCalories);
 
         DateTime dT = DateTime.now();
         int dayOfWeek = dT.getDayOfWeek();
         int today = dT.getMonthOfYear()*1000000 + dT.getDayOfMonth()*10000 + dT.getYear();
-        try {
         int caloriesEntered = DDao.getDateCalories(today);
         int weightEntered = DDao.getDateWeight(today);
         DayStats newEntry = new DayStats();
-            newEntry.DayOfWeek = dayOfWeek;
-            newEntry.DayDate = today;
-            newEntry.DayWeight = weightEntered;
-            newEntry.DayCalories = Integer.parseInt(txt.getText().toString()) + caloriesEntered;
-            newEntry.DayFat = 0;
-            newEntry.DayCarbs = 0;
-            newEntry.DayProtein = 0;
+        newEntry.DayOfWeek = dayOfWeek;
+        newEntry.DayDate = today;
+        newEntry.DayWeight = weightEntered;
+        newEntry.DayCalories = Integer.parseInt(txt.getText().toString()) + caloriesEntered;
+        newEntry.DayFat = 0;
+        newEntry.DayCarbs = 0;
+        newEntry.DayProtein = 0;
 
-            if (caloriesEntered == 0 && weightEntered == 0) {  //No entry has been made for today
-                DDao.insertWeight(newEntry);
-            } else {  //an entry has been made to add weight but not calories
-                newEntry.Fdate = DDao.getDateKey(today);
+        if (caloriesEntered == 0 && weightEntered == 0) {  //No entry has been made for today
+            DDao.insertWeight(newEntry);
+        } else {  //an entry has been made for today
+            newEntry.Fdate = DDao.getDateKey(today);
 
-                DDao.updateNutrition(Integer.parseInt(txt.getText().toString()) + caloriesEntered, 0, 0, 0, today);
-            }
-
-        txt = findViewById(R.id.eatOutput);
-        txt.setText(String.valueOf(DDao.getDateCalories(today)));
-        } catch(Exception e){
-            System.out.println(e);
-        }*/
+            DDao.updateNutrition(Integer.parseInt(txt.getText().toString()) + caloriesEntered, 0, 0, 0, today);
+        }
     }
 }
